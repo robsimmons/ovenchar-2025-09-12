2cfd330ad42f52d39a4e7d969be880130969f889
Rob Simmons <robsimmons@gmail.com>
2025-09-13 14:08:49 -0400

    Make example 1 bigger

--- Toy/Example1.lean
+++ Toy/Example1.lean
@@ -9,15 +9,47 @@ def baz := #tree .node (.leaf .c) (.node (.leaf .a) (.leaf .b))
 def large := #tree .node
   (.node
     (.node
-      (.node (.node (.leaf .a) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .c) (.node (.leaf .a) (.leaf .b))))
-      (.node (.node (.leaf .c) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .c) (.node (.leaf .a) (.leaf .d)))))
+      (.node
+        (.node (.node (.leaf .a) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .c) (.node (.leaf .a) (.leaf .b))))
+        (.node (.node (.leaf .c) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .c) (.node (.leaf .a) (.leaf .d)))))
+      (.node
+        (.node (.node (.leaf .c) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .b) (.node (.leaf .a) (.leaf .b))))
+        (.node (.node (.leaf .a) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .b) (.node (.leaf .a) (.leaf .d))))))
     (.node
-      (.node (.node (.leaf .c) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .b) (.node (.leaf .a) (.leaf .b))))
-      (.node (.node (.leaf .a) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .b) (.node (.leaf .a) (.leaf .d))))))
+      (.node
+        (.node (.node (.leaf .a) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .c) (.node (.leaf .a) (.leaf .b))))
+        (.node (.node (.leaf .c) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .b) (.node (.leaf .a) (.leaf .d)))))
+      (.node
+        (.node (.node (.leaf .c) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .b) (.node (.leaf .a) (.leaf .b))))
+        (.node (.node (.leaf .c) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .b) (.node (.leaf .a) (.leaf .d)))))))
   (.node
     (.node
-      (.node (.node (.leaf .a) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .c) (.node (.leaf .a) (.leaf .b))))
-      (.node (.node (.leaf .c) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .b) (.node (.leaf .a) (.leaf .d)))))
+      (.node
+        (.node
+          (.node (.node (.leaf .a) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .c) (.node (.leaf .a) (.leaf .b))))
+          (.node (.node (.leaf .c) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .c) (.node (.leaf .a) (.leaf .d)))))
+       (.node
+          (.node (.node (.leaf .c) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .b) (.node (.leaf .a) (.leaf .b))))
+          (.node (.node (.leaf .a) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .b) (.node (.leaf .a) (.leaf .d))))))
+      (.node
+        (.node
+          (.node (.node (.leaf .a) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .c) (.node (.leaf .a) (.leaf .b))))
+          (.node (.node (.leaf .c) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .b) (.node (.leaf .a) (.leaf .d)))))
+        (.node
+          (.node (.node (.leaf .c) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .b) (.node (.leaf .a) (.leaf .b))))
+          (.node (.node (.leaf .c) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .b) (.node (.leaf .a) (.leaf .d)))))))
     (.node
-      (.node (.node (.leaf .c) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .b) (.node (.leaf .a) (.leaf .b))))
-      (.node (.node (.leaf .c) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .b) (.node (.leaf .a) (.leaf .d))))))
+      (.node
+        (.node
+          (.node (.node (.leaf .a) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .c) (.node (.leaf .a) (.leaf .b))))
+          (.node (.node (.leaf .c) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .c) (.node (.leaf .a) (.leaf .d)))))
+       (.node
+          (.node (.node (.leaf .c) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .b) (.node (.leaf .a) (.leaf .b))))
+          (.node (.node (.leaf .a) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .b) (.node (.leaf .a) (.leaf .d))))))
+      (.node
+        (.node
+          (.node (.node (.leaf .a) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .c) (.node (.leaf .a) (.leaf .b))))
+          (.node (.node (.leaf .c) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .b) (.node (.leaf .a) (.leaf .d)))))
+        (.node
+          (.node (.node (.leaf .c) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .b) (.node (.leaf .a) (.leaf .b))))
+          (.node (.node (.leaf .c) (.node (.leaf .a) (.leaf .b))) (.node (.leaf .b) (.node (.leaf .a) (.leaf .d))))))))
